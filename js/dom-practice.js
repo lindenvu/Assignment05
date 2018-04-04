f2083e33f6f0c1d2c47035dbdab6fdac371810d9
lindenvu <github@lindenvu.com>
2018-04-03 19:17:21 -0700

    Update 2

---
+++ js/dom-practice.js
@@ -0,0 +1,45 @@
+/*eslint-env browser*/
+
+//STEP 1
+function writeMessage(str) {
+    "use strict";
+    window.alert(str + "I have been clicked!");
+}
+
+//STEP 2
+var btn2 = window.document.getElementById("step2");
+btn2.setAttribute("onclick", "writeMessage('Button 2: ')");
+
+//STEP 3
+var btn3 = window.document.getElementById("step3");
+var writeMessageButton3 = function () { "use strict"; window.alert("Button 3: I have been clicked!"); };
+btn3.addEventListener("click", writeMessageButton3);
+
+//STEP 4
+var btn4 = window.document.getElementById("step4");
+btn4.addEventListener("click", function () { "use strict"; window.alert("Button 4: I have been clicked!"); });
+
+//STEP 5
+
+//STEP 6
+var link6 = window.document.getElementById("redirect");
+link6.setAttribute("href", "#");
+link6.setAttribute("onclick", "return writeMessage('Link 6: ')");
+
+//STEP 7
+var btn7 = window.document.getElementById("step7");
+btn7.addEventListener("click", function () {
+    "use strict";
+    var inputText = window.document.getElementById("collectInput").value;
+    window.alert("Button 7: " + inputText);
+    btn7.disabled = true;
+});
+
+//STEP 8
+var btn8 = window.document.getElementById("step8");
+btn8.addEventListener("click", function () {
+    window.open("newpage.html", "newpage", "width=300,height=300");
+});
+//STEP 9
+
+//STEP 10
---
+++ js/employee-management.js
@@ -0,0 +1,67 @@
+/*eslint-env browser*/
+
+var $ = function (id) {
+    "use strict";
+    return window.document.getElementById(id);
+};
+
+var employee_list = [["Zak Ruvalcaba", "Engineering", 3333],
+                 ["Sally Smith", "Quality Assurance", 3424],
+                 ["Fred Franklin", "Sales", 3346],
+                 ["John Smith", "Marketing", 3232],
+                 ["Jane Caruthers", "Finance", 3451]];
+
+
+function printArray() {
+    "use strict";
+    var i, html = "<tr><td>Showing " + employee_list.length + " Employees</td></tr>";
+    html += "<tr><td>Name</td><td>Title</td><td>Extension</td><td>&nbsp;</td></tr>";
+    for (i = 0; i < employee_list.length; i += 1) {
+        html += "<tr><td>" + employee_list[i][0] + "</td><td>" + employee_list[i][1] + "</td><td>" + employee_list[i][2] + "</td><td><input type=\"button\" id=\"" + i + "\" value=\"delete\" onclick=\"delEmployee(" + i + ")\"></td></tr>";
+    }
+    $("employees").innerHTML = html;
+}
+
+
+
+
+var add_employee = $("add");
+add_employee.addEventListener("click", function () {
+    "use strict";
+    var errorMessage = "<span style=\"color:red\">This field is required.</span>", blank = "&nbsp;";
+    if ($("name").value === "" || $("title").value  === "" || $("extension").value  === "") {
+        if ($("name").value === "") {$("msg1").innerHTML = errorMessage; } else {$("msg1").innerHTML =  blank; }
+        if ($("title").value === "") {$("msg2").innerHTML = errorMessage; } else {$("msg2").innerHTML =  blank; }
+        if ($("extension").value === "") {$("msg3").innerHTML = errorMessage; } else {$("msg3").innerHTML =  blank; }
+    } else {
+        employee_list[employee_list.length] = [$("name").value, $("title").value, $("extension").value];
+        printArray();
+        $("msg1").innerHTML =  blank;
+        $("msg2").innerHTML =  blank;
+        $("msg3").innerHTML =  blank;
+        $("name").value = "";
+        $("title").value = "";
+        $("extension").value = "";
+    }
+});
+
+function delEmployee(id) {
+    "use strict";
+    var new_list = [], z, x = 0, y = parseInt(id, 10);
+    for (z = 0; z < employee_list.length; z += 1) {
+        if (z !== y) {
+            new_list[x] = employee_list[z];
+            x += 1;
+        }
+    }
+    employee_list = new_list;
+    printArray();
+}
+
+
+var showEmployees = function () {
+    "use strict";
+    printArray();
+};
+
+window.addEventListener("load", showEmployees);
